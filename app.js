2ec45731ece61660a68bbfbce52246eae390dc36
Paul Niemczyk <mrpndev@gmail.com>
2026-03-09 19:09:11 -0400

    challenge

--- app.js
+++ app.js
@@ -45,3 +45,21 @@ console.log(listItems)
 	* go thru the properties and see how you may be able to access the text
 	* if the text includes the word 'milk', change its color to rebeccapurple
 */
+
+// listItems.forEach(item => console.log(item)) // TypeError .forEach not a fx
+// this is because HTMLCollections look like arrays but are not arrays
+
+for (item of listItems) {
+	if (item.textContent.includes("milk")) {
+		console.log(item.textContent, item.textContent.includes("milk"))
+		// item.style.color = "rebeccapurple"
+		item.classList.add("target")
+	}
+}
+// TODO: challenge to check if a substring exists within a string
+
+// ? .textContent is a property which holds text between opening and closing tag
+
+
+
+
