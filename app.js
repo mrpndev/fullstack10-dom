aae97f61c73e5c32e61c21510e6ab0f417b342ae
Paul Niemczyk <mrpndev@gmail.com>
2026-03-09 20:18:13 -0400

    dom last challenge

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log("Intro to DOM")
 	* and all whitespace and text within
 
 	* link to DOM tree:
-	https://en.wikipedia.org/wiki/Document_Object_Model#/media/File:DOM-model.svg
+	
 */
 
 // DOM object in a nutshell
@@ -60,6 +60,92 @@ for (item of listItems) {
 
 // ? .textContent is a property which holds text between opening and closing tag
 
+// ? .querySelector()
+
+// ? allows you to grab anything and everything any way you want
+
+let queryByTag = document.querySelector("nav")
+console.log(queryByTag)
+let queryById = document.querySelector("#nav-bar")
+console.log(queryById)
+let queryByClass = document.querySelector(".nav-bar")
+console.log(queryByClass)
+let queryByCompoundSelectors = document.querySelector("html > body nav.nav-bar")
+console.log(queryByCompoundSelectors)
+
+// ? .querySelectorAll()
+
+// ? like querySelector but for many
+let navLinks = document.querySelectorAll(".nav-links")
+
+// ? NodeLists allow us to use array methods on them!
+navLinks.forEach(link => link.style.fontSize = "16pt")
+
+/* 
+	? HTMLCollection vs NodeList
+	* HTMLCollection is a live list (shows new elements)
+	* HTMLCollection cannot have array methods be used on it
+	* NodeList is a static list (does not show new elements)
+	* NodeList can have array methods be used on it
+*/
+
+// ? Creating elements using DOM
+
+// 1. create the element
+let p = document.createElement("p")
+// 2. modify the element attributes to your desired outcome
+p.textContent = new Date().getFullYear()
+console.log(p)
+// 3. append the newly created element to the DOM object
+footer.appendChild(p) // this appends p to the footer
+
+// ? How to add element in order rather than to the end
+let ul = document.querySelector("ul")
+// ul.after(p)
+// document.body.insertBefore(p, footer)
+
+let htmlCollection = document.getElementsByClassName("list-items")
+let nodeList = document.querySelectorAll(".list-items")
+console.log(htmlCollection, nodeList)
+
+// creating new li element and appending to the ul
+let li = document.createElement("li")
+li.textContent = "Clean bedroom"
+li.className = "list-items"
+ul.appendChild(li)
+
+console.log(htmlCollection, nodeList)
+
+for (i of htmlCollection) {
+	// i.style.color = "blue"
+}
+
+for (i of nodeList) {
+	i.style.color = "salmon"
+}
+
+/* 
+	* HTMLCollection will update the color of the newly created element
+	* NodeList will treat it as if the new li element doesn't exist
+*/
+
+/* 
+	? Challenge
+	* use DOM to change the background color to "lightgray"
+*/
+
+document.body.style.backgroundColor = "lightgray"
 
 
+let domUrl = "https://en.wikipedia.org/wiki/Document_Object_Model#/media/File:DOM-model.svg"
+
+/* 
+	? Final Chalenge
+	* create an img element
+		* using html and using a method to grab it OR
+		* using DOM from the beginning
+	* assign that wiki link to the src attr
+	* add the element to the body
+	* style it so that it's not huge
+*/
 
